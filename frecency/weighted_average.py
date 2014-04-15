5b801d22884d79ac1110917b475c2e209d791908
Michael J.T. O'Kelly <michael@plangrid.com>
2014-04-15 02:50:33 -0700

    Added 'offset' value to WeightedAverage() to permit sampling of negative values

--- frecency/weighted_average.py
+++ frecency/weighted_average.py
@@ -13,33 +13,50 @@ by Michael J.T. O'Kelly, 2014-04-09
 import frecency
 
 
+OFFSET_DEFAULT = -1e-5  # Constant to help handle negative samples
+
+
 class WeightedAverage():
     """Exponentially weighted average of a variable sampled over time.
     NOTE: The underlying data structure requires that all samples be greater
-    than zero."""
+    than the initially given 'offset' value."""
     # TODO: Arbitrary values could potentially be accepted by keeping
     # an offset value
-    def __init__(self, timescale=frecency.DEFAULT_TIMESCALE):
+    def __init__(self, timescale=frecency.DEFAULT_TIMESCALE, offset=OFFSET_DEFAULT):
         """* *timescale* is the halflife of events, in seconds.  With the default (24 hours)
-        an event now counts twice as much as an event 24 hours ago."""
+            an event now counts twice as much as an event 24 hours ago.
+        * *offset* is a value lower (by at least a little) than any sample
+            to be observed."""
         self.n_sum = frecency.Frecency(timescale)  # Weight accumulator
         self.x_sum = frecency.Frecency(timescale)  # Variable accumulator
         self.x2_sum = frecency.Frecency(timescale)  # Variable squared accumulator
         self.timescale = timescale
+        self.offset = offset
+
+    def _apply_sample_offset(self, sample):
+        """Applies the current offset value to the sample, adjusting it
+        if necessary, and returns offset_sample.
+
+        Since Frecency() objects can only handle positive values, we
+        maintain an offset value that allows us to correct
+        for negative samples in get_mean_std_uncertainty()."""
+        if sample <= self.offset:
+            raise ValueError("Samples must be greater than the initialized offset value", sample, self.offset)
+        offset_sample = sample - self.offset
+        return offset_sample
 
     def add_sample(self, sample, weight=1.0, event_time=None):
         """Incorporate a new sample into the weighted average.
 
         * *sample* is one sampled value of the variable.  (It must be
-            greater than zero.)
+            greater than 'offset'.)
         * *weight* is the relative weight assigned to this sample.
         * *event_time* is the epoch time of this sample (if different from 'now')
         """
-        if sample <= 0.:
-            raise ValueError("Samples must be greater than zero", sample)
+        offset_sample = self._apply_sample_offset(sample)
         self.n_sum.increment(weight, event_time=event_time)
-        self.x_sum.increment(sample, event_time=event_time)
-        self.x2_sum.increment(sample ** 2, event_time=event_time)
+        self.x_sum.increment(offset_sample, event_time=event_time)
+        self.x2_sum.increment(offset_sample ** 2, event_time=event_time)
 
     def get_mean_std_uncertainty(self):
         """Returns (mean, std, uncertainty) tuple of present best estimates.
@@ -52,26 +69,29 @@ class WeightedAverage():
         present_x = self.x_sum.get_present_weight()
         present_x2 = self.x2_sum.get_present_weight()
 
-        mean = present_x / present_n
+        offset_mean = present_x / present_n
         # Calculate the variance from the X^2 cumulant
         # (http://en.wikipedia.org/wiki/Variance#Definition)
-        variance = present_x2 / present_n - mean ** 2
+        variance = present_x2 / present_n - offset_mean ** 2
         # http://en.wikipedia.org/wiki/Variance#Sample_variance
         # TODO: Sample bias correction.  This was attempted:
         # variance = present_n / (present_n - 1.)
         # but that didn't work in the exponential weighting framework
         std = variance ** 0.5
         uncertainty = std / present_n ** 0.5
+        mean = offset_mean + self.offset  # Unapply the offset correction
+
         return (mean, std, uncertainty)
 
 
 if __name__=='__main__':
     import random
     import time
-    w = WeightedAverage(timescale=0.01)
+    w = WeightedAverage(timescale=0.01, offset=-10.)
     start_time = time.time()
     for i in range(100000):
-        sample = random.gauss(10., 1.)
+        # sample = random.gauss(10., 1.)
+        sample = random.gauss(0., 1.)
         w.add_sample(sample)
     print "time passed:", time.time() - start_time
-    print w.get_mean_and_std()
+    print w.get_mean_std_uncertainty()
