b6127e3bff601fd6d4026f61d1523412c4f47967
Rachel Domagalski <rachel@cobaltrobotics.com>
2019-03-29 14:18:07 -0700

    fix import error for different pip versions

--- setup.py
+++ setup.py
@@ -11,7 +11,10 @@ try:
 except ImportError:
     from distutils.core import setup, find_packages
 
-from pip.req import parse_requirements
+try: # for pip >= 10
+    from pip._internal.req import parse_requirements
+except ImportError: # for pip <= 9.0.3
+    from pip.req import parse_requirements
 
 if sys.argv[-1] == 'publish':
     os.system('python setup.py sdist upload')
